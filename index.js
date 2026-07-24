02230dfecd1ff419a14f38876ebf610e40a8961c
Adrian Prendas <prendas.adrian@gmail.com>
2026-07-23 18:29:35 -0600

    new mongodb url and connection option

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ var app = require('./app');
 var port = process.env.PORT || 8080;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://jose:macros98@ds159344.mlab.com:59344/sudoku', { useMongoClient: true})
-//mongoose.connect('mongodb://localhost:27017/sudoku', { useMongoClient: true})  
+//mongoose.connect('mongodb://jose:macros98@ds159344.mlab.com:59344/sudoku', { useMongoClient: true})
+mongoose.connect('mongodb://root:example@localhost:27017/sudoku', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    authSource: 'admin'
+})  
     .then(() => {
         console.log('La conexion a la base de datos se realizo correctamente...');
         app.listen(port, () => {
